a5b94bc350a6c4e8ba250bc94c10012b7e7c3968
darwin3750 <damedel@mymail.mapua.edu.ph>
2021-02-19 00:59:44 +0800

    add slideshow template

--- assets/main.js
+++ assets/main.js
@@ -1,2 +1,19 @@
+---
+---
+
 window.onload = () => {
+  if(document.title.slice(document.title.indexOf("|") + 2) == "Home"){
+    let homeCarousel = new Flickity(".carousel");
+    resizeFlickity(null, homeCarousel);
+    homeCarousel.on('settle', ()=>{
+      resizeFlickity(null, homeCarousel);
+    });
+  }
+}
+
+//general carousel resizing
+function resizeFlickity(flickityId = null, flickityObject = null){
+  let targetCarousel = flickityId == null ? flickityObject: new Flickity(flickityId);
+  targetCarousel.watchCSS();
+  targetCarousel.resize();
 }
