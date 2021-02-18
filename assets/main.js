8a506f5451fbbf36022bdc03acb81f3d25b243f3
darwin3750 <damedel@mymail.mapua.edu.ph>
2021-02-19 02:27:06 +0800

    make navbar sticky

--- assets/main.js
+++ assets/main.js
@@ -1,19 +1,16 @@
----
----
-
 window.onload = () => {
-  if(document.title.slice(document.title.indexOf("|") + 2) == "Home"){
-    let homeCarousel = new Flickity(".carousel");
-    resizeFlickity(null, homeCarousel);
-    homeCarousel.on('settle', ()=>{
-      resizeFlickity(null, homeCarousel);
-    });
-  }
-}
+  
+  //make sticky header consume space
+  const headerResizeObserver = new ResizeObserver(entries => 
+    document.querySelector("#header-bottom").style.paddingTop = entries[0].target.clientHeight + "px"
+  );headerResizeObserver.observe(document.querySelector("#header"));
 
-//general carousel resizing
+}//window.onload end
+
+//function for general carousel resizing
 function resizeFlickity(flickityId = null, flickityObject = null){
   let targetCarousel = flickityId == null ? flickityObject: new Flickity(flickityId);
   targetCarousel.watchCSS();
   targetCarousel.resize();
 }
+
