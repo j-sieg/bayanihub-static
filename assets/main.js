c3f580eae5c94175606c36c192a14e09a7084334
darwin3750 <damedel@mymail.mapua.edu.ph>
2021-02-24 15:17:45 +0800

    add js to add active class to the corresponding active header link

--- assets/main.js
+++ assets/main.js
@@ -5,6 +5,14 @@ window.onload = () => {
     document.querySelector("#header-bottom").style.paddingTop = entries[0].target.clientHeight + "px"
   );headerResizeObserver.observe(document.querySelector("#header"));
 
+  //Header: add active class to active page
+  [].slice.call(document.querySelector("#navigation-links ul").children).forEach(function(el){
+    if(el.firstElementChild.textContent == document.title.slice(document.title.indexOf("|") + 2)){
+      el.firstElementChild.classList.add("active");
+    }else{
+      el.firstElementChild.classList.remove("active");
+    };
+  });
 }//window.onload end
 
 //function for general carousel resizing
